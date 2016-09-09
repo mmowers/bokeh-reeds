47adb55cf1bc1736d1eb906868c188bd107b7648
Matt Mowers <Matthew.Mowers@nrel.gov>
2016-09-09 00:04:43 -0500

    plot hiding ability

--- templates/scripts.js
+++ templates/scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     $('body').on('click', '#modelid_scenarios_heading', function(){
         $('#modelid_scenarios').toggle();
-    })
+    });
+    $('body').on('click', '#modelid_scenarios input', function(){
+        var label = $(this).parent().text()
+        var checked = $(this).is(':checked');
+        var index = $('#modelid_scenarios input').index($(this));
+        $( '.bk-plot-layout:eq('+index+')').toggle();
+    });
 });
