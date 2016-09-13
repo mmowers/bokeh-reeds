55eafbf430e807ab4ce6df968a417ff7a999f85c
Matt Mowers <Matthew.Mowers@nrel.gov>
2016-09-13 03:08:50 -0500

    Add combined chart

--- main.py
+++ main.py
@@ -55,8 +55,8 @@ hierarchy = copy.deepcopy(hierarchy_input)
 hierarchy_column_names = ['i','n','r','rnew','rto','censusregions','st','in','country','custreg','readin']
 hierarchy.columns = hierarchy_column_names
 
-colors = ['#5e4fa2', '#3288bd', '#66c2a5', '#abdda4', '#e6f598', '#ffffbf', '#fee08b', '#fdae61', '#f46d43', '#d53e4f', '#9e0142']
-colors = colors*10
+scenario_colors = ['#5e4fa2', '#3288bd', '#66c2a5', '#abdda4', '#e6f598', '#ffffbf', '#fee08b', '#fdae61', '#f46d43', '#d53e4f', '#9e0142']
+
 
 data_obj = {}
 for result in gdx_structure.keys():
@@ -85,7 +85,8 @@ widgets = col.OrderedDict((
     ('region', bmw.Select(value='USA', options=hierarchy['country'].unique().tolist(), id='region')),
     ('year', bmw.Select(value='All years', options=['All years'] + [str(x) for x in years_full], id='year')),
     ('timeslice', bmw.Select(value='All timeslices', options=['All timeslices','H1','H2','H3'], id='timeslice')),
-    ('scale_axes', bmw.RadioButtonGroup(labels=["Sync Axes", "Scale Independently"])),
+    ('scale_axes', bmw.RadioButtonGroup(labels=['Sync Axes', 'Scale Independently'])),
+    ('rerender', bmw.Button(label='Re-render', button_type='success')),
 ))
 
 def initialize():
@@ -109,14 +110,25 @@ def initialize():
         plot['figure'].xaxis.major_label_orientation = 'vertical'
         plot['figure'].xaxis.major_label_standoff = 25
         plot_list['scenarios'][scenario_name] = plot
+    
+    combined_plot = {
+        'figure': bp.Figure(toolbar_location='right', tools='save,pan,box_zoom,reset', width=250, height=250),
+        'series': [],
+    }
+    combined_plot['figure'].title.text = 'Combined'
+    combined_plot['figure'].xaxis.major_label_orientation = 'vertical'
+    combined_plot['figure'].xaxis.major_label_standoff = 25
+    plot_list['combined'] = combined_plot
+
     fill_plots()
 
 def fill_plots():
-    for scenario in scenarios:
-        fill_plot(scenario)
-
-def fill_plot(scenario):
     result = widgets['result'].value
+    for scenario in scenarios:
+        fill_plot(scenario, result)
+    build_combined_chart(result)
+
+def fill_plot(scenario, result):
     df_base = data_obj[result]['scenarios'][scenario]['dataframe']
     if type(df_base) is int:
         df_base = get_dataframe(scenario, result)
@@ -124,6 +136,14 @@ def fill_plot(scenario):
     df = filter_dataframe(df_base)
     if widgets['charttype'].value == 'Stacked Area':
         fill_stacked_areas(df, scenario)
+
+def build_combined_chart(result):
+    df_base = data_obj[result]['combined']['dataframe']
+    if type(df_base) is int:
+        df_base = get_combined_dataframe(result)
+        data_obj[result]['combined']['dataframe'] = df_base
+    df = filter_dataframe(df_base, combined=True)
+    build_combined_line_chart(df)
 
 def fill_stacked_areas(df, scenario):
     x_values = np.hstack((df.index, df.index[::-1])).tolist()
@@ -136,16 +156,31 @@ def fill_stacked_areas(df, scenario):
         else:
             plot['series'].append(plot['figure'].patch(x_values, y_values[j], alpha = 0.8, color = display_techs[series_name]['color'], line_color = None, line_width = None, name = series_name))
 
+def build_combined_line_chart(df):
+    x_values = df.index.tolist()
+    y_values = df.transpose().values.tolist()
+
+    plot = plot_list['combined']
+    for j, series_name in enumerate(df.columns.values.tolist()):
+        if j < len(plot['series']):
+            plot['series'][j].data_source.data['y'] = y_values[j]
+        else:
+            plot['series'].append(plot['figure'].line(x_values, y_values[j], alpha = 0.8, color = scenario_colors[j], line_width = 2, name = series_name))
+    plot['figure'].x_range.start = min(x_values)
+    plot['figure'].x_range.end = max(x_values)
+    plot['figure'].y_range.start = min([min(a) for a in y_values]+[0])
+    plot['figure'].y_range.end = max([max(a) for a in y_values])
+
 def get_axis_ranges(scenario, x_values, y_values):
     plot = plot_list['scenarios'][scenario]
     plot['x_min'] = min(x_values)
     plot['x_max'] = max(x_values)
-    plot['y_min'] = min([min(a) for a in y_values])
+    plot['y_min'] = min([min(a) for a in y_values]+[0])
     plot['y_max'] = max([max(a) for a in y_values])
 
 
-def get_dataframe(scenario, result_type):
-    gdx_result = gdx_structure[result_type]
+def get_dataframe(scenario, result):
+    gdx_result = gdx_structure[result]
     df = gdxpds.to_dataframe(runs_path + scenario + '\\gdxfiles\\' + gdx_result['file'], gdx_result['param'])[gdx_result['param']]
     df.columns = gdx_result['columns']
     multi_index_iterables = []
@@ -159,7 +194,6 @@ def get_dataframe(scenario, result_type):
     if 'reg' in gdx_result:
         multi_index_iterables.append(regions_full[gdx_result['reg']])
         multi_index_names.append(gdx_result['reg'])
-        hier = hierarchy.drop_duplicates(gdx_result['reg'])
     if 'year' in df.columns:
         df['year'] = df['year'].apply(pd.to_numeric)
         multi_index_iterables.append(years_full)
@@ -171,10 +205,18 @@ def get_dataframe(scenario, result_type):
     df_index = pd.MultiIndex.from_product(multi_index_iterables, names=multi_index_names)
     df = df.set_index(multi_index_names).reindex(df_index).reset_index()
     df = df.fillna(0)
-    
-    if 'reg' in gdx_result:
-        df = pd.merge(df, hier, how='left', on=gdx_result['reg'])
     return df
+
+def get_combined_dataframe(result):
+    comb_df = pd.DataFrame()
+    gdx_result = gdx_structure[result]
+    grouped_cols = [gdx_result['reg'], 'year']
+    for scenario in scenarios:
+        df = data_obj[result]['scenarios'][scenario]['dataframe']
+        df = df.groupby(grouped_cols, as_index=False, sort=False)['value'].sum()
+        df['scenario'] = scenario
+        comb_df = pd.concat([comb_df, df])
+    return comb_df
 
 def stack_lists(raw_lists):
     last_stack = [0]*len(raw_lists[0])
@@ -185,11 +227,18 @@ def stack_lists(raw_lists):
         last_stack = next_stack
     return stacked_lists
 
-def filter_dataframe(df_base):
-    df = df_base[df_base[widgets['regtype'].value].isin([widgets['region'].value])]
-    df = df.groupby([widgets['series'].value, widgets['xaxis'].value], as_index=False, sort=False)['value'].sum()
-    df = df.pivot(index=widgets['xaxis'].value, columns=widgets['series'].value, values='value')
-    df = df[display_techs.keys()]
+def filter_dataframe(df_base, combined=False):
+    gdx_result = gdx_structure[widgets['result'].value]
+    hier = hierarchy.drop_duplicates(gdx_result['reg'])
+    df = pd.merge(df_base, hier, how='left', on=gdx_result['reg'])
+    df = df[df[widgets['regtype'].value].isin([widgets['region'].value])]
+    if not combined:
+        df = df.groupby([widgets['series'].value, widgets['xaxis'].value], as_index=False, sort=False)['value'].sum()
+        df = df.pivot(index=widgets['xaxis'].value, columns=widgets['series'].value, values='value')
+        df = df[display_techs.keys()]
+    else:
+        df = df.groupby(['scenario', widgets['xaxis'].value], as_index=False, sort=False)['value'].sum()
+        df = df.pivot(index=widgets['xaxis'].value, columns='scenario', values='value')
     return df
 
 def sync_axes():
@@ -213,7 +262,6 @@ def scale_axes_independently():
 
 def general_filter_update(attrname, old, new):
     fill_plots()
-    widgets['scale_axes'].active = None
 
 def update_regtype(attrname, old, new):
     widgets['region'].options = hierarchy[widgets['regtype'].value].unique().tolist()
@@ -223,13 +271,19 @@ def scale_axes(new):
     if new == 0: sync_axes()
     elif new == 1: scale_axes_independently()
 
+def rerender():
+    fill_plots()
+    scale_axes(widgets['scale_axes'].active)
+
 widgets['result'].on_change('value', general_filter_update)
 widgets['region'].on_change('value', general_filter_update)
 widgets['regtype'].on_change('value', update_regtype)
 widgets['scale_axes'].on_click(scale_axes)
+widgets['rerender'].on_click(rerender)
 
 initialize()
 plots = [p['figure'] for p in plot_list['scenarios'].values()]
+plots.append(plot_list['combined']['figure'])
 filters = bl.widgetbox(widgets.values(), width=300, id='widgets_section')
 plots_display = bl.column(plots, width=1000, id='plots_section')
 bio.curdoc().add_root(bl.row([filters, plots_display]))
